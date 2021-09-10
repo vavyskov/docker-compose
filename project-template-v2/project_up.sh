4cb1b3c9a79821bc5b48cafb0f56ce08b1ab6f45
Tomas Danek <tomas.danek@vavyskov.cz>
2021-09-10 14:04:23 +0200

    URLs info

--- project-template-v2/project_up.sh
+++ project-template-v2/project_up.sh
@@ -14,6 +14,14 @@ else
         "$(tput setaf 1)" "$(tput sgr 0)"
         exit
     fi
+    ## Get PROJECT_BASE_URL variable (exists, not comment, not empty)
+    if [ -n "$(cat < .env | grep PROJECT_BASE_URL= | sed '/^#/d' | cut -d= -f2 | xargs)" ]; then
+        PROJECT_BASE_URL=$(cat < .env | grep PROJECT_BASE_URL= | cut -d= -f2 | xargs)
+    else
+        printf "\r\n%s'PROJECT_BASE_URL' variable is not set.%s\r\n\r\n" \
+        "$(tput setaf 1)" "$(tput sgr 0)"
+        exit
+    fi
 fi
 
 ## Network
@@ -22,3 +30,39 @@ docker network create ${COMPOSE_PROJECT_NAME}_network
 
 ## Create and start containers
 docker-compose up -d
+
+## Show URLs
+echo ""
+tput smul
+echo "Project URLs:"
+tput rmul
+echo "http://${PROJECT_BASE_URL}"
+echo "http://adminer.${PROJECT_BASE_URL}"
+echo "http://mailcatcher.${PROJECT_BASE_URL}"
+echo ""
+
+## Notes
+## tput smul # set underline
+## tput rmul # remove underline
+##
+## tput smso # set bold on
+## tput rmso # remove bold
+##
+## tput setaf 1 #red
+## tput setaf 2 #green
+##
+## tput cup 0 0 # move to pos 0,0
+##
+## tput reset
+
+## Open URLs in browser
+## Linux
+#xdg-open "http://mailcatcher.${PROJECT_BASE_URL}"
+#xdg-open "http://adminer.${PROJECT_BASE_URL}"
+#xdg-open "http://${PROJECT_BASE_URL}"
+## macOS
+#open "http://mailcatcher.${PROJECT_BASE_URL}"
+#open "http://adminer.${PROJECT_BASE_URL}"
+#open "http://${PROJECT_BASE_URL}"
+## Windows
+#start "http://mailcatcher.${PROJECT_BASE_URL}" "http://adminer.${PROJECT_BASE_URL}" "http://${PROJECT_BASE_URL}"
